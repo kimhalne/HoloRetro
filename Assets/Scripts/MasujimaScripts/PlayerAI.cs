c329a144f479fe4237c5a2cb8ca426bffec41aa6
MasujimaRyohei <nushibeatbox@gmail.com>
2017-06-04 08:35:59 +0900

    Commit Masujima's files

---
+++ Assets/Scripts/MasujimaScripts/PlayerAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerAI : MonoBehaviour
+{
+    [SerializeField]
+    private bool isAuto;
+    [SerializeField]
+    private float range = 25.0f;
+    NavMeshAgent a;
+    // Use this for initialization
+    void Start()
+    {
+        a = GetComponent<NavMeshAgent>();
+        if (isAuto)
+            StartCoroutine(SetNextDestination());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SetNextDestination()
+    {
+        while (true)
+        {
+            if (a.pathPending || a.remainingDistance > 0.1f)
+                yield return null;
+
+            a.destination = range * Random.insideUnitCircle;
+        }
+    }
+}
