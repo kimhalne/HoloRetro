65acd8ecab31965d695e28ea541164ef4e2708e2
Miyaura <cafe.kobe@gmail.com>
2017-06-04 10:18:03 +0900

    朝時点の全体的なプロトロジックの追加。 Main.unityを使用すると今朝の状態を確認できます。 HoleEffect    - 穴のあける動き用 KittyGenerate - 定期的にキャラクターを生成する用 KittyMove     - キャラクターをカメラにとりあえず近づけるスクリプト SpawnHoles    - エアタップで地面に穴をあける nekonokawari.prefab - キャラクタの代わりにしているCube Hole.prefab         - 穴のオブジェクト
    KittyGenerateには生成するキャラクタのPrefabをパラメータに与えてください。
    （現在はnekonokawari.prefab)
    SpawnHolesには生成する穴のPrefabをパラメータに与えてください。
    （現在はHole.prefab）

---
+++ Assets/HoloRetro/Scripts/HoleEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 落とし穴のエフェクト用
+/// 最初に0.3近くまでスケールを拡張し、５秒後に閉じる。
+/// </summary>
+public class HoleEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform.localScale = Vector3.one*0.01f;
+	}
+
+    private float timer = 0;
+	// Update is called once per frame
+	void Update () {
+        if (transform.localScale.z < 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (timer > 5)
+        {
+            transform.localScale -= Vector3.one * Time.deltaTime * 0.6f;
+        }
+        else if (transform.localScale.z < 0.30f)
+        { 
+		transform.localScale += Vector3.one*Time.deltaTime*0.6f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/HoloRetro/Scripts/KittyGenerate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittyGenerate : MonoBehaviour
+{
+
+
+    public GameObject kitty;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private float counter = 0;
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    counter += Time.deltaTime;
+
+	    if (counter > 5)
+	    {
+	        GameObject.Instantiate(kitty);
+	        kitty.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 5f;
+	        counter = 0;
+	    }
+
+
+	}
+}
