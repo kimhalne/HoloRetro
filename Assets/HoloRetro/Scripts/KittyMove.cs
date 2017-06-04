e82a5616db9eecd655a7a345aba0e01239165029
Miyaura <cafe.kobe@gmail.com>
2017-06-04 12:49:27 +0900

    あけた穴に衝突判定を付けました。 Cubeが穴に入るとサイズが縮んでオブジェクトが削除されます。

--- Assets/HoloRetro/Scripts/KittyMove.cs
+++ Assets/HoloRetro/Scripts/KittyMove.cs
@@ -8,13 +8,33 @@ public class KittyMove : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
+    private bool isHit = false;
 	// Update is called once per frame
-	void Update () {
-		gameObject.transform.LookAt(Camera.main.transform);
-        var heading = Camera.main.transform.position - gameObject.transform.position;
-        var distance = heading.magnitude;
-        var direction = heading / distance; // This is now the normalized direction.
-        gameObject.transform.position += direction * Time.deltaTime * 0.5f;
-	    }
+    void Update()
+    {
+        
+        gameObject.transform.LookAt(Camera.main.transform);
+        if (!isHit)
+        {
+            var heading = Camera.main.transform.position - gameObject.transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance; // This is now the normalized direction.
+            gameObject.transform.position += direction * Time.deltaTime * 0.5f;
+        }
+    }
+
+    // 衝突している間呼ばれ続ける  
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Hole"))
+        {
+            //isHit = true;
+            if (transform.localScale.z < 0)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale -= Vector3.one * Time.deltaTime * 0.6f;
+        }
+    }
 }
